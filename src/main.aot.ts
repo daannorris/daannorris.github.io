b9bb4c9154bd9d6a30cf1c7898b36b68c53fe5ee
Daniel Carrillo <daniel.carrillo.olvera@gmail.com>
2017-07-13 15:35:36 -0500

    Enable Production Mode for Angular

--- src/main.aot.ts
+++ src/main.aot.ts
@@ -1,5 +1,5 @@
-import { platformBrowser }    from '@angular/platform-browser';
-
+import { platformBrowser } from '@angular/platform-browser';
+import { enableProdMode } from '@angular/core';
 import { AppModuleNgFactory } from './aot/app/app.module.ngfactory';
-
+enableProdMode();
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
